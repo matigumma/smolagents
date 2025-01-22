61d16ff191d2de554eee7bf334c825256deada7d
Mati <matiasgumma@gmail.com>
2025-01-22 18:24:39 -0300

    base

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import os
 from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
 from huggingface_hub import login
+from dotenv import load_dotenv  # Add this import at the top
+import os
 
-hf_api_key = os.getenv("HF_API_KEY")
+load_dotenv()
+
+hf_api_key = os.getenv("HF_API_TOKEN")
 if hf_api_key is None:
-    raise EnvironmentError("HF_API_KEY environment variable not set.")
+    raise EnvironmentError("HF_API_TOKEN environment variable not set.")
 login(hf_api_key)
 
 agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=HfApiModel())
